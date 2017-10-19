ecd07bdd3ee6d3b01c53179a3a5c19569b356b9f
Andrew Lechowicz <andrew@alecho.com>
2017-10-18 20:54:07 -0400

    Ping and pong GenServers iex> {_, p1} = Lift.start_link() iex> {_, p2} = Lift.start_link() iex> Lift.ping(p1, p2)

--- lib/elevator.ex
+++ lib/elevator.ex
@@ -1,18 +1,29 @@
 defmodule Lift do
-  @moduledoc """
-  Documentation for Lift.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link() do
+    GenServer.start_link(__MODULE__, %{})
+  end
 
-  ## Examples
+  def ping(pid, pid2) do
+    GenServer.cast(pid, {:ping, pid2})
+  end
 
-      iex> Lift.hello
-      :world
+  def pong(pid, pid2) do
+    GenServer.cast(pid, {:pong, pid2})
+  end
 
-  """
-  def hello do
-    :world
+  def handle_cast({:ping, other}, state) do
+    :timer.sleep 500
+    IO.puts("Ping -->")
+    GenServer.cast(other, {:pong, self()})
+    {:noreply, state}
+  end
+
+  def handle_cast({:pong, other}, state) do
+    :timer.sleep 500
+    IO.puts("<-- Pong")
+    GenServer.cast(other, {:ping, self()})
+    {:noreply, state}
   end
 end
--- test/elevator_test.exs
+++ test/elevator_test.exs
@@ -1,8 +1,26 @@
 defmodule LiftTest do
   use ExUnit.Case
+  require GenServer
   doctest Lift
 
-  test "greets the world" do
-    assert Lift.hello() == :world
+  test "starts with a pid" do
+    {:ok, ping} = Lift.start_link()
+    assert is_pid(ping)
+  end
+
+  test "responds to :ping" do
+    {:ok, pid} = Lift.start_link()
+    test = Lift.ping(pid)
+    assert test == :pong
+    test = Lift.ping(pid)
+    assert test == :pong
+  end
+
+  test "responds to :pong" do
+    {:ok, pid} = Lift.start_link()
+    test = Lift.pong(pid)
+    assert test == :ping
+    test = Lift.pong(pid)
+    assert test == :ping
   end
 end
